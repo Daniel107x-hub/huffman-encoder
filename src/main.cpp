019ba31b83d37b2fa02e152a84e348c97ce9f686
Daniel Mejia <Daniel.Mejia2@Honeywell.com>
2024-02-28 17:44:39 -0600

    Added update for file reading

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by H460351 on 2/8/2024.
+//
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int getCharFrequency(string inputString){
+    return 5;
+}
+
+int main(){
+}
